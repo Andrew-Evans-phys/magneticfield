292ddb8fe34cbf7e6c815cf346b9f66adb09ca6d
hazeydailyhill <76240464+hazeydailyhill@users.noreply.github.com>
2021-12-19 12:18:46 -0800

    Updated files to vectorized files

---
+++ cpp_mag_field/run.bat
@@ -0,0 +1,6 @@
+ECHO OFF
+cls
+ECHO Running code :)
+g++ *.cpp -o main
+C:/Users/haley/Documents/magneticfield/cpp_mag_field/main
+C:/Users/haley/AppData/Local/Programs/Python/Python310/python.exe c:/Users/haley/Documents/magneticfield/cpp_mag_field/data_display.py
